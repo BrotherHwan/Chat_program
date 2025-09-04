fedadc4d2e702bdc9974117a4ab297ea27bd3911
benhuh <maga_h@naver.com>
2025-09-04 14:12:56 +0900

    채팅 서버 1차 완료

--- chat_server_linux.c
+++ chat_server_linux.c
@@ -58,3 +58,45 @@ int main(int argc, char* argv[])
     close(serv_sock);
     return 0;
 }
+
+void* handle_clnt(void* arg)
+{
+    int clnt_sock=*((int*)arg);
+    int str_len=0, i;
+    char msg[BUF_SIZE];
+
+    while((str_len=read(clnt_sock, msg, sizeof(msg)))!=0)
+        send_msg(msg, str_len);
+
+    pthread_mutex_lock(&mutx);
+    for(i=0; i<clnt_cnt; i++)
+    {
+        if(clnt_sock==clnt_socks[i])
+        {
+            while(i++<clnt_cnt-1)
+                clnt_socks[i]=clnt_socks[i+1];
+            break;
+        }
+    }
+
+    clnt_cnt--;
+    pthread_mutex_unlock(&mutx);
+    close(clnt_sock);
+    return NULL;
+}
+
+void send_msg(char *msg, int len)
+{
+    int i;
+    pthread_mutex_lock(&mutx);
+    for(i=0; i<clnt_cnt; i++)
+        write(clnt_socks[i], msg, len);
+    pthread_mutex_unlock(&mutx);
+}
+
+void error_handling(char* msg)
+{
+    fputs(msg, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
